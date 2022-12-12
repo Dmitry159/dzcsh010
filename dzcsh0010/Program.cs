59bfdd9462bbd610b424dcea6f700ce7c00f1ac5
Dmitry <godivl@mail.ru>
2022-12-12 17:13:20 +0500

    dzcsh010

---
+++ dzcsh0010/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// на входе трехзначное число на выходе показывает вторую цифру
+
+Console.WriteLine("Введите трехзначное число: ");
+int numberA=int.Parse(Console.ReadLine());
+numberA=numberA%100/10;
+Console.WriteLine(numberA);
